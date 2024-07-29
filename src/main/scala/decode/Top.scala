f7d3259959c5168ec61c01033ee883970a88fcd9
NozomiOshi <NozomiOshi@outlook.com>
2024-07-29 22:33:51 +0800

    feat(fetch): implement fetch
    implement basic fetch in CPU core

---
+++ src/main/scala/decode/Top.scala
@@ -0,0 +1,13 @@
+package decode
+
+import chisel3._
+
+class Top extends Module {
+    val exit = IO(Output(Bool()))
+
+    val core = Module(new Core)
+    val mem  = Module(new Memory)
+    mem.imem <> core.omem
+    
+    exit := core.exit
+}
