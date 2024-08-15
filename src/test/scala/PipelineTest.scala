4d6d75aa3d2afd43bc7d4ee98dd0ee69977ea0b4
NozomiOshi <NozomiOshi@outlook.com>
2024-08-15 15:34:52 +0800

    test: add Data Hazard chiseltest

--- src/test/scala/PipelineTest.scala
+++ src/test/scala/PipelineTest.scala
@@ -6,13 +6,23 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class HazardTest extends AnyFlatSpec with ChiselScalatestTester {
+    val configBrHazard = () => {
+        val cpu = new Top
+        cpu.mem.loadMemoryFromHexFile("src/test/resources/hex/brhazard.hex")
+        cpu
+    }
+    val configWbHazard = () => {
+        val cpu = new Top
+        cpu.mem.loadMemoryFromHexFile("src/test/resources/hex/wbhazard.hex")
+        cpu
+    }
+    val configExHazard = () => {
+        val cpu = new Top
+        cpu.mem.loadMemoryFromHexFile("src/test/resources/hex/exhazard.hex")
+        cpu
+    }
     behavior of "cpu"
-        it should "work through hex" in {
-            val configBrHazard = () => {
-                val cpu = new Top
-                cpu.mem.loadMemoryFromHexFile("src/test/resources/hex/brhazard.hex")
-                cpu
-            }
+        it should "work through Branch Hazard" in {
             test(configBrHazard()) { dut =>
                 while(!dut.exit.peek().litToBoolean) {
                     dut.clock.step()
@@ -20,4 +30,22 @@ class HazardTest extends AnyFlatSpec with ChiselScalatestTester {
                 dut.clock.step()    
             }
         }
+    behavior of "cpu"
+        it should "work through Data Hazard: Wb" in {
+            test(configWbHazard()) { dut =>
+                while(!dut.exit.peek().litToBoolean) {
+                    dut.clock.step()
+                }
+                dut.clock.step()    
+            }
+        }
+    behavior of "cpu"
+        it should "work through Data Hazard: Ex" in {
+            test(configExHazard()) { dut =>
+                while(!dut.exit.peek().litToBoolean) {
+                    dut.clock.step()
+                }
+                dut.clock.step()    
+            }
+        }            
 }
